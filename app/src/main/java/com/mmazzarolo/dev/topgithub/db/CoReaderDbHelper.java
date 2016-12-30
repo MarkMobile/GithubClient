39a9c1820a7a54ca7966813a3c80724822f600a7
Arison <784602719@qq.com>
2016-12-30 18:28:02 +0800

    V1.0项目初始化...

---
+++ app/src/main/java/com/mmazzarolo/dev/topgithub/db/CoReaderDbHelper.java
@@ -0,0 +1,20 @@
+package com.mmazzarolo.dev.topgithub.db;
+
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by Arison on 2016/12/30.
+ */
+public class CoReaderDbHelper extends SQLiteOpenHelper {
+    
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
